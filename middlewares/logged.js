775c1eb98171131b1038fee39088935d3826cb6d
Renzo-Bravo <renzobravogomez@gmail.com>
2025-10-27 16:22:42 -0300

    Create file middlewares and archives

---
+++ middlewares/logged.js
@@ -0,0 +1,10 @@
+function logged (req,res, next) {
+    if(req.session.userLogged){
+        return next ();
+    }
+    return res.redirect("/users/login");
+}
+
+module.exports = logged;
+
+//Sesión de Usuarios
