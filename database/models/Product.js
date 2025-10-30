9d889567929fbdd135a0478057c49d8de9997e36
Renzo-Bravo <renzobravogomez@gmail.com>
2025-10-30 15:22:43 -0300

    Update Prod model

--- database/models/Product.js
+++ database/models/Product.js
@@ -70,7 +70,10 @@ module.exports = (sequelize, DataTypes) => {
 
   Product.associate = (models) => {
     Product.belongsTo(models.Brand, { foreignKey: "brand_id", as: "marca" });
-    Product.belongsTo(models.Category, { foreignKey: "category_id", as: "categoria" });
+    Product.belongsTo(models.Category, {
+      foreignKey: "category_id",
+      as: "categoria",
+    });
     Product.belongsTo(models.Color, { foreignKey: "color_id", as: "colores" });
     Product.belongsTo(models.Gender, { foreignKey: "gender_id", as: "genero" });
     Product.belongsTo(models.Size, { foreignKey: "size_id", as: "talle" });
